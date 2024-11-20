0d6567e91bacf3922d023161a84d41d08ad8da5c
its-sky <smc9919@naver.com>
2024-11-20 09:55:12 +0900

    init: project 초기화

---
+++ src/main/kotlin/org/smc/push/PushApplication.kt
@@ -0,0 +1,11 @@
+package org.smc.push
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PushApplication
+
+fun main(args: Array<String>) {
+	runApplication<PushApplication>(*args)
+}
---
+++ src/test/kotlin/org/smc/push/PushApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.smc.push
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PushApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
